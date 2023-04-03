89c74648be19810d13cd80f95fea279c607cddfd
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2023-04-03 03:52:49 -0300

    Character animations demo can now select specific animations

--- demos/character_animations/main.lua
+++ demos/character_animations/main.lua
@@ -7,7 +7,7 @@ function surge.pre_loop()
     sophia_actor = surge.actor.new(
         "character_animations/resources/spritesheet.png",                        -- Sprite sheet image file
         "character_animations/resources/spritesheet.sad",                        -- Animations file
-        6,                                                                       -- First animation index
+        5,                                                                       -- First animation index
         16.0, 48.0, 0.0,                                                         -- Anchor
         surge.config.window_width / 2.0,  surge.config.window_height / 2.0, 0.0, -- Position
         5.0, 5.0, 1.0                                                            -- Scale
@@ -19,7 +19,21 @@ function surge.key_event(key, action, mods)
         surge.actor.scale(sophia_actor, 1.5, 1.5, 0.0)
     elseif key == surge.input.keyboard.key.PAGE_DOWN and action == surge.input.action.PRESS then
         surge.actor.scale(sophia_actor, 1.0/1.5, 1.0/1.5, 0.0)
-    end 
+    elseif key == surge.input.keyboard.key.ONE and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 5, false)
+    elseif key == surge.input.keyboard.key.TWO and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 6, true)
+    elseif key == surge.input.keyboard.key.THREE and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 2, false)
+    elseif key == surge.input.keyboard.key.FOUR and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 1, true)
+    elseif key == surge.input.keyboard.key.FIVE and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 4, false)
+    elseif key == surge.input.keyboard.key.SIX and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 0, true)
+    elseif key == surge.input.keyboard.key.SEVEN and action == surge.input.action.PRESS then
+        surge.actor.change_anim(sophia_actor, 3, false)
+    end
 end
 
 function surge.mouse_button_event(button, action, mods)
