f826c794622d05916c8c378242f881040313228b
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2023-08-23 01:14:47 -0300

    Added color to date

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,8 @@
+#[cfg(feature = "log_color")]
 macro_rules! log_info {
     ($msg:literal) => {
         println!(
-            "[{}] \x1b[36m[Thread ID: {}]\x1b[m \x1b[1m\x1b[32mSURGE Info:\x1b[m {}",
+            "\x1b[94m[{}]\x1b[m \x1b[36m[Thread ID: {}]\x1b[m \x1b[1m\x1b[32mSURGE Info:\x1b[m {}",
             chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
             std::thread::current().id().as_u64(),
             $msg
@@ -11,7 +12,7 @@ macro_rules! log_info {
     ($fmt_string:tt, $($args:tt)*) => {
         println!(
             concat!(
-                "[{}] \x1b[36m[Thread ID: {}]\x1b[m \x1b[1m\x1b[32mSURGE Info:\x1b[m ",
+                "\x1b[94m[{}]\x1b[m \x1b[36m[Thread ID: {}]\x1b[m \x1b[1m\x1b[32mSURGE Info:\x1b[m ",
                 $fmt_string
             ),
             chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
