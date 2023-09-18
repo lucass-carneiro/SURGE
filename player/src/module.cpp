2720893c797bbae74267c3888b1e127f94292f05
lucass-carneiro <lucas.t.s.carneiro@gmail.com>
2023-09-17 22:55:39 -0300

    Added module loading on windows

--- player/src/module.cpp
+++ player/src/module.cpp
@@ -5,7 +5,9 @@
 surge::module::owned_module::owned_module(const char *path) noexcept
     : module_handle{LoadLibrary(path)},
       on_load_ptr{reinterpret_cast<on_load_t>(GetProcAddress(module_handle, "on_load"))},
-      on_unload_ptr{reinterpret_cast<on_unload_t>(GetProcAddress(module_handle, "on_unload"))} {}
+      on_unload_ptr{reinterpret_cast<on_unload_t>(GetProcAddress(module_handle, "on_unload"))} {
+  // TODO handle load errors
+}
 
 surge::module::owned_module::~owned_module() {
   on_unload();
