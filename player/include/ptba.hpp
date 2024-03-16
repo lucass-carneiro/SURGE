03a835eff89497dd362314e058643d5eddc33ba7
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-03-16 19:12:23 -0300

    Work on ptba

--- player/include/ptba.hpp
+++ player/include/ptba.hpp
@@ -13,35 +13,20 @@ namespace surge::atom::buffers {
 // https://registry.khronos.org/OpenGL-Refpages/gl4/html/glBufferStorage.xhtml
 // https://registry.khronos.org/OpenGL-Refpages/gl4/html/glClientWaitSync.xhtml
 // https://registry.khronos.org/OpenGL-Refpages/gl4/html/glMapBufferRange.xhtml
-// https://www.bilibili.com/video/BV1V64y1h7UJ/
+// https://gdcvault.com/play/1020791/
 // https://www.slideshare.net/CassEveritt/approaching-zero-driver-overhead (85)
 
-struct size_aware_fence {
-  usize written_bytes{0};
-  GLsync fence{};
-};
-
-struct ptba_options {
-  usize max_elements{1024};
-  usize buffers{3};
+struct locked_range {
+  usize write_start{0};
+  usize write_size{0};
+  GLsync lock{nullptr};
 };
 
 // Persistant triple buffered array
-template <typename T> class ptba {
+template <typename T, usize num_buffers = 3> class ptba {
 public:
-  // TODO Copy and move
-  ptba(const ptba_options &opts)
-      : elements_per_buffer{opts.max_elements},
-        num_buffers{opts.buffers},
-        single_buffer_size{elements_per_buffer * element_size},
-        total_size{opts.buffers * single_buffer_size} {
-
-    log_info("Creating PTBA\n"
-             "  Elms. per buffer: %lu\n"
-             "  Elm. size: %lu B\n"
-             "  Num. buffers: %lu B\n"
-             "  Total size: %lu B",
-             elements_per_buffer, element_size, opts.buffers);
+  ptba(usize capacity) {
+    const auto total_size{capacity * sizeof(T) * num_buffers};
 
     const GLbitfield map_flags{GL_MAP_WRITE_BIT | GL_MAP_PERSISTENT_BIT | GL_MAP_COHERENT_BIT};
     const GLbitfield alloc_flags{map_flags | GL_DYNAMIC_STORAGE_BIT};
@@ -59,13 +44,32 @@ public:
   auto operator=(ptba &&) noexcept -> ptba & = delete;
 
 private:
-  static constexpr auto element_size{sizeof(T)};
-  const usize elements_per_buffer{0};
-  const usize num_buffers{0};
-  const usize single_buffer_size{0};
-  const usize total_size{0};
+  /*
+  after writing/drawing(?)place fence that knows location and size of write
+  a few frames later when trying to write to the same region, wait for lock
+  does the region i'm about to write overlaps with a region prev. written? If so
+  */
+  void lock_range(usize start, usize size) noexcept {
+    if (range_lock.lock) {
+      glDeleteSync(range_lock.lock);
+    }
+    range_lock.lock = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
+    range_lock.write_start = start;
+    range_lock.write_size = size;
+  }
 
-  usize dest_head{0};
+  void wait_for_locked_range() noexcept {
+    if (range_lock.lock) {
+      while (true) {
+        const auto wait_return = glClientWaitSync(range_lock.lock, GL_SYNC_FLUSH_COMMANDS_BIT, 1);
+        if (wait_return == GL_ALREADY_SIGNALED || wait_return == GL_CONDITION_SATISFIED)
+          return;
+      }
+    }
+  }
+
+  locked_range range_lock{};
+  usize write_head{0};
 
   GLuint handle{0};
   std::span<std::byte> data;
