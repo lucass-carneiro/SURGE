51898abe3a110d69655f6ab8142c2af61fc38e5a
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-04-01 23:19:35 -0300

    Work on club level

--- DTU/design/resources/tiled/base.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="base" tilewidth="80" tileheight="92" spacing="5" tilecount="4" columns="4">
- <image source="base_blocks.png" width="335" height="92"/>
-</tileset>
