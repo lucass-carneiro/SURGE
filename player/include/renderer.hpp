3bec550529227ce41b340e0511ff53c183e75292
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-04-18 12:08:14 -0300

    Added utilities to compile and link compute shaders

--- player/include/renderer.hpp
+++ player/include/renderer.hpp
@@ -36,7 +36,12 @@ auto create_shader_program(const char *vertex_shader_path,
                            const char *fragment_shader_path) noexcept
     -> tl::expected<GLuint, error>;
 
+auto create_compute_shader(const char *compute_shader_path) noexcept -> tl::expected<GLuint, error>;
+
 void destroy_shader_program(GLuint program) noexcept;
+
+void dispatch_compute(GLuint program, GLuint x, GLuint y, GLuint z) noexcept;
+void dispatch_compute(GLuint program, GLuint x, GLuint y, GLuint z, GLbitfield barriers) noexcept;
 
 namespace uniforms {
 
--- player/src/shaders.cpp
+++ player/src/shaders.cpp
@@ -22,7 +22,8 @@ static auto load_and_compile_shader(const char *p, GLenum shader_type) noexcept
 
   files::file file{};
 
-  if (shader_type == GL_VERTEX_SHADER || shader_type == GL_FRAGMENT_SHADER) {
+  if (shader_type == GL_VERTEX_SHADER || shader_type == GL_FRAGMENT_SHADER
+      || shader_type == GL_COMPUTE_SHADER) {
     file = files::load_file(p, true);
   } else {
     log_error("Unrecognized shader type %u", shader_type);
@@ -136,6 +137,62 @@ static auto link_shader_program(GLuint vertex_shader_handle, GLuint fragment_sha
   }
 }
 
+static auto link_single_shader(GLuint shader_handle, bool destroy_shaders = true) noexcept
+    -> tl::expected<GLuint, surge::error> {
+
+#if defined(SURGE_BUILD_TYPE_Profile) && defined(SURGE_ENABLE_TRACY)
+  ZoneScopedN("surge::renderer::link_shader_program");
+  TracyGpuZone("GPU surge::renderer::link_shader_program");
+#endif
+
+  log_info("Linking shader handle %u", shader_handle);
+
+  // Get a program object.
+  GLuint program_handle_tmp{glCreateProgram()};
+
+  // Attach our shaders to our program
+  glAttachShader(program_handle_tmp, shader_handle);
+
+  // Link our program
+  glLinkProgram(program_handle_tmp);
+
+  GLint is_linked{0};
+  glGetProgramiv(program_handle_tmp, GL_LINK_STATUS, &is_linked);
+
+  if (is_linked == GL_FALSE) {
+    GLint max_length = 0;
+    glGetProgramiv(program_handle_tmp, GL_INFO_LOG_LENGTH, &max_length);
+
+    // The max_length includes the NULL character
+    GLsizei returnd_info_size{0};
+    std::array<GLchar, SURGE_OPENGL_ERROR_BUFFER_SIZE> info_log{};
+
+    glGetProgramInfoLog(program_handle_tmp, SURGE_OPENGL_ERROR_BUFFER_SIZE, &returnd_info_size,
+                        info_log.data());
+
+    // Cleanup
+    glDetachShader(program_handle_tmp, shader_handle);
+    glDeleteProgram(program_handle_tmp);
+
+    log_info("Failed to link shader handles %u to create program:\n  %s", shader_handle,
+             info_log.data());
+    return tl::unexpected(surge::error::shader_link_error);
+
+  } else {
+    glDetachShader(program_handle_tmp, shader_handle);
+
+    if (destroy_shaders) {
+      log_info("Destroying shaders handles %u", shader_handle);
+      glDeleteShader(shader_handle);
+    }
+
+    log_info("Shader handle %u linked succesfully. Program handle: %u", shader_handle,
+             program_handle_tmp);
+
+    return program_handle_tmp;
+  }
+}
+
 auto surge::renderer::create_shader_program(const char *vertex_shader_path,
                                             const char *fragment_shader_path) noexcept
     -> tl::expected<GLuint, error> {
@@ -159,6 +216,21 @@ auto surge::renderer::create_shader_program(const char *vertex_shader_path,
   return link_shader_program(*vertex_shader_handle, *fragment_shader_handle);
 }
 
+auto surge::renderer::create_compute_shader(const char *compute_shader_path) noexcept
+    -> tl::expected<GLuint, error> {
+#if defined(SURGE_BUILD_TYPE_Profile) && defined(SURGE_ENABLE_TRACY)
+  ZoneScopedN("surge::renderer::create_compute_shader");
+#endif
+
+  const auto compute_shader_handle{load_and_compile_shader(compute_shader_path, GL_COMPUTE_SHADER)};
+
+  if (!compute_shader_handle) {
+    return tl::unexpected{compute_shader_handle.error()};
+  }
+
+  return link_single_shader(*compute_shader_handle);
+}
+
 void surge::renderer::destroy_shader_program(GLuint program) noexcept {
 #if defined(SURGE_BUILD_TYPE_Profile) && defined(SURGE_ENABLE_TRACY)
   ZoneScopedN("surge::renderer::destroy_shader_program");
@@ -166,4 +238,15 @@ void surge::renderer::destroy_shader_program(GLuint program) noexcept {
 #endif
   log_info("Destroying shader program handle %u", program);
   glDeleteProgram(program);
+}
+
+void surge::renderer::dispatch_compute(GLuint program, GLuint x, GLuint y, GLuint z) noexcept {
+  glUseProgram(program);
+  glDispatchCompute(x, y, z);
+}
+
+void surge::renderer::dispatch_compute(GLuint program, GLuint x, GLuint y, GLuint z,
+                                       GLbitfield barriers) noexcept {
+  dispatch_compute(program, x, y, z);
+  glMemoryBarrier(barriers);
 }
