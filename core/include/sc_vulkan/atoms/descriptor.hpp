4efec003c500de3ac3451ae5e8269b3e36b31c5e
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2025-02-08 00:34:48 -0600

    Updated vulkan descriptor abstractions

--- core/include/sc_vulkan/atoms/descriptor.hpp
+++ core/include/sc_vulkan/atoms/descriptor.hpp
@@ -15,28 +15,55 @@
 
 namespace surge::vk_atom::descriptor {
 
-struct desc_pool_size_ratio {
+struct pool_size_ratio {
   VkDescriptorType type{};
   float ratio{};
 };
 
-class desc_alloc {
+class allocator {
 private:
-  VkDescriptorPool pool{};
+  static constexpr u32 max_sets_per_pool{4096};
+  u32 sets_per_pool{0};
+
+  vector<pool_size_ratio> ratios{};
+  vector<VkDescriptorPool> full_pools{};
+  vector<VkDescriptorPool> ready_pools{};
+
+  auto get_pool(VkDevice device) -> VkDescriptorPool;
+
+  auto create_pool(VkDevice device, u32 set_count, std::span<pool_size_ratio> pool_ratios)
+      -> VkDescriptorPool;
 
 public:
-  auto init_pool(renderer::vk::context ctx, u32 max_sets,
-                 std::span<desc_pool_size_ratio> pool_ratios) -> tl::expected<void, error>;
+  void init(renderer::vk::context ctx, u32 initial_sets, std::span<pool_size_ratio> pool_ratios);
 
-  auto clear_descriptors(renderer::vk::context ctx) -> tl::expected<void, error>;
+  void clear_pools(renderer::vk::context ctx);
 
-  void destroy_pool(renderer::vk::context ctx);
+  void destroy_pools(renderer::vk::context ctx);
 
-  auto allocate(renderer::vk::context ctx, VkDescriptorSetLayout layout)
+  auto allocate(renderer::vk::context ctx, VkDescriptorSetLayout layout, void *pNext = nullptr)
       -> tl::expected<VkDescriptorSet, error>;
 };
 
-class desc_builder {
+class writer {
+private:
+  deque<VkDescriptorBufferInfo> buffer_infos{};
+  deque<VkDescriptorImageInfo> image_infos{};
+  vector<VkWriteDescriptorSet> writes{};
+
+public:
+  void write_buffer(u32 binding, VkBuffer buffer, size_t size, size_t offset,
+                    VkDescriptorType type);
+
+  void write_image(u32 binding, VkImageView image, VkSampler sampler, VkImageLayout layout,
+                   VkDescriptorType type);
+
+  void clear();
+
+  void update_set(renderer::vk::context ctx, VkDescriptorSet set);
+};
+
+class builder {
 private:
   vector<VkDescriptorSetLayoutBinding> bindings;
 
@@ -50,10 +77,6 @@ public:
 
 auto draw_img_desc_info(renderer::vk::context ctx) -> VkDescriptorImageInfo;
 void destroy_desc_layout(renderer::vk::context ctx, VkDescriptorSetLayout layout);
-
-void update_desc_sets(renderer::vk::context ctx, u32 write_count,
-                      const VkWriteDescriptorSet *desc_writes, u32 copy_count,
-                      const VkCopyDescriptorSet *desc_copies);
 
 } // namespace surge::vk_atom::descriptor
 
--- core/src/sc_vulkan/atoms/descriptor.cpp
+++ core/src/sc_vulkan/atoms/descriptor.cpp
@@ -7,68 +7,174 @@
 
 #include <vulkan/vk_enum_string_helper.h>
 
-auto surge::vk_atom::descriptor::desc_alloc::init_pool(renderer::vk::context ctx, u32 max_sets,
-                                                       std::span<desc_pool_size_ratio> pool_ratios)
-    -> tl::expected<void, error> {
-  vector<VkDescriptorPoolSize> pool_sizes{};
+auto surge::vk_atom::descriptor::allocator::get_pool(VkDevice device) -> VkDescriptorPool {
+  VkDescriptorPool new_pool{};
+
+  if (ready_pools.size() != 0) {
+    new_pool = ready_pools.back();
+    ready_pools.pop_back();
+  } else {
+    new_pool = create_pool(device, sets_per_pool, ratios);
+
+    sets_per_pool *= 2;
+
+    if (sets_per_pool > max_sets_per_pool) {
+      log_info("Max descriptor sets per pool limit ({}) reached. Ignoring resize request",
+               max_sets_per_pool);
+      sets_per_pool = max_sets_per_pool;
+    }
+  }
+
+  return new_pool;
+}
+
+auto surge::vk_atom::descriptor::allocator::create_pool(VkDevice device, u32 set_count,
+                                                        std::span<pool_size_ratio> pool_ratios)
+    -> VkDescriptorPool {
+  vector<VkDescriptorPoolSize> poolSizes{};
+
   for (const auto &ratio : pool_ratios) {
-    pool_sizes.push_back(VkDescriptorPoolSize{
-        .type = ratio.type, .descriptorCount = static_cast<u32>(ratio.ratio * max_sets)});
+    poolSizes.push_back(VkDescriptorPoolSize{
+        .type = ratio.type, .descriptorCount = static_cast<u32>(ratio.ratio * set_count)});
   }
 
   VkDescriptorPoolCreateInfo pool_info{};
   pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
   pool_info.flags = 0;
-  pool_info.pNext = nullptr;
-  pool_info.maxSets = max_sets;
-  pool_info.poolSizeCount = static_cast<u32>(pool_sizes.size());
-  pool_info.pPoolSizes = pool_sizes.data();
+  pool_info.maxSets = set_count;
+  pool_info.poolSizeCount = static_cast<u32>(poolSizes.size());
+  pool_info.pPoolSizes = poolSizes.data();
 
-  const auto result{
-      vkCreateDescriptorPool(ctx->device, &pool_info, renderer::vk::get_alloc_callbacks(), &pool)};
+  VkDescriptorPool new_pool{};
+  vkCreateDescriptorPool(device, &pool_info, nullptr, &new_pool);
+  return new_pool;
+}
 
-  if (result != VK_SUCCESS) {
-    log_error("Unable to create descriptor pool: {}", string_VkResult(result));
-    return tl::unexpected{vk_descriptor_pool_init};
+void surge::vk_atom::descriptor::allocator::init(renderer::vk::context ctx, u32 initial_sets,
+                                                 std::span<pool_size_ratio> pool_ratios) {
+  ratios.clear();
+
+  for (auto &r : pool_ratios) {
+    ratios.push_back(r);
   }
 
-  return {};
+  auto new_pool{create_pool(ctx->device, initial_sets, pool_ratios)};
+  sets_per_pool = initial_sets * 2;
+  ready_pools.push_back(new_pool);
 }
 
-auto surge::vk_atom::descriptor::desc_alloc::clear_descriptors(renderer::vk::context ctx)
-    -> tl::expected<void, error> {
-  const auto result{vkResetDescriptorPool(ctx->device, pool, 0)};
-  if (result != VK_SUCCESS) {
-    log_error("Unable to reset descriptor pool: {}", string_VkResult(result));
-    return tl::unexpected{vk_descriptor_pool_reset};
+void surge::vk_atom::descriptor::allocator::clear_pools(renderer::vk::context ctx) {
+  for (auto &p : ready_pools) {
+    vkResetDescriptorPool(ctx->device, p, 0);
   }
 
-  return {};
+  for (auto &p : full_pools) {
+    vkResetDescriptorPool(ctx->device, p, 0);
+    ready_pools.push_back(p);
+  }
+
+  full_pools.clear();
 }
 
-void surge::vk_atom::descriptor::desc_alloc::destroy_pool(renderer::vk::context ctx) {
-  vkDestroyDescriptorPool(ctx->device, pool, renderer::vk::get_alloc_callbacks());
+void surge::vk_atom::descriptor::allocator::destroy_pools(renderer::vk::context ctx) {
+  for (auto &p : ready_pools) {
+    vkDestroyDescriptorPool(ctx->device, p, nullptr);
+  }
+
+  ready_pools.clear();
+
+  for (auto &p : full_pools) {
+    vkDestroyDescriptorPool(ctx->device, p, nullptr);
+  }
+
+  full_pools.clear();
 }
 
-auto surge::vk_atom::descriptor::desc_alloc::allocate(renderer::vk::context ctx,
-                                                      VkDescriptorSetLayout layout)
+auto surge::vk_atom::descriptor::allocator::allocate(renderer::vk::context ctx,
+                                                     VkDescriptorSetLayout layout, void *pNext)
     -> tl::expected<VkDescriptorSet, error> {
-  VkDescriptorSetAllocateInfo ai = {};
+
+  // Get or create a pool to allocate from
+  auto pool_to_use{get_pool(ctx->device)};
+
+  VkDescriptorSetAllocateInfo ai{};
+  ai.pNext = pNext;
   ai.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-  ai.pNext = nullptr;
-  ai.descriptorPool = pool;
+  ai.descriptorPool = pool_to_use;
   ai.descriptorSetCount = 1;
   ai.pSetLayouts = &layout;
 
   VkDescriptorSet ds{};
-  const auto result{vkAllocateDescriptorSets(ctx->device, &ai, &ds)};
+  auto result{vkAllocateDescriptorSets(ctx->device, &ai, &ds)};
 
-  if (result != VK_SUCCESS) {
-    log_error("Unable to allocate descriptor sets: {}", string_VkResult(result));
-    return tl::unexpected{vk_descriptor_set_alloc};
+  // allocation failed. Try again
+  if (result == VK_ERROR_OUT_OF_POOL_MEMORY || result == VK_ERROR_FRAGMENTED_POOL) {
+    full_pools.push_back(pool_to_use);
+
+    pool_to_use = get_pool(ctx->device);
+    ai.descriptorPool = pool_to_use;
+
+    result = vkAllocateDescriptorSets(ctx->device, &ai, &ds);
+
+    if (result != VK_SUCCESS) {
+      log_error("Unable to allocate descriptor sets: {}", string_VkResult(result));
+      return tl::unexpected{vk_descriptor_set_alloc};
+    }
   }
 
+  ready_pools.push_back(pool_to_use);
+
   return ds;
+}
+
+void surge::vk_atom::descriptor::writer::write_buffer(u32 binding, VkBuffer buffer, size_t size,
+                                                      size_t offset, VkDescriptorType type) {
+  auto &info{buffer_infos.emplace_back(
+      VkDescriptorBufferInfo{.buffer = buffer, .offset = offset, .range = size})};
+
+  VkWriteDescriptorSet write{};
+  write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+  write.pNext = nullptr;
+  write.dstBinding = binding;
+  write.dstSet = VK_NULL_HANDLE; // Left empty for now until we need to write it
+  write.descriptorCount = 1;
+  write.descriptorType = type;
+  write.pBufferInfo = &info;
+
+  writes.push_back(write);
+}
+
+void surge::vk_atom::descriptor::writer::write_image(u32 binding, VkImageView image,
+                                                     VkSampler sampler, VkImageLayout layout,
+                                                     VkDescriptorType type) {
+  auto &info = image_infos.emplace_back(
+      VkDescriptorImageInfo{.sampler = sampler, .imageView = image, .imageLayout = layout});
+
+  VkWriteDescriptorSet write{};
+  write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+  write.pNext = nullptr;
+  write.dstBinding = binding;
+  write.dstSet = VK_NULL_HANDLE; // left empty for now until we need to write it
+  write.descriptorCount = 1;
+  write.descriptorType = type;
+  write.pImageInfo = &info;
+
+  writes.push_back(write);
+}
+
+void surge::vk_atom::descriptor::writer::clear() {
+  buffer_infos.clear();
+  image_infos.clear();
+  writes.clear();
+}
+
+void surge::vk_atom::descriptor::writer::update_set(renderer::vk::context ctx,
+                                                    VkDescriptorSet set) {
+  for (auto &write : writes) {
+    write.dstSet = set;
+  }
+
+  vkUpdateDescriptorSets(ctx->device, static_cast<u32>(writes.size()), writes.data(), 0, nullptr);
 }
 
 auto surge::vk_atom::descriptor::draw_img_desc_info(renderer::vk::context ctx)
@@ -80,7 +186,7 @@ auto surge::vk_atom::descriptor::draw_img_desc_info(renderer::vk::context ctx)
   return info;
 }
 
-void surge::vk_atom::descriptor::desc_builder::add_binding(u32 binding, VkDescriptorType type) {
+void surge::vk_atom::descriptor::builder::add_binding(u32 binding, VkDescriptorType type) {
   VkDescriptorSetLayoutBinding b{};
   b.binding = binding;
   b.descriptorCount = 1;
@@ -89,11 +195,12 @@ void surge::vk_atom::descriptor::desc_builder::add_binding(u32 binding, VkDescri
   bindings.push_back(b);
 }
 
-void surge::vk_atom::descriptor::desc_builder::clear() { bindings.clear(); }
+void surge::vk_atom::descriptor::builder::clear() { bindings.clear(); }
 
-auto surge::vk_atom::descriptor::desc_builder::build(
-    renderer::vk::context ctx, VkShaderStageFlags shader_stages, void *pNext,
-    VkDescriptorSetLayoutCreateFlags flags) -> tl::expected<VkDescriptorSetLayout, error> {
+auto surge::vk_atom::descriptor::builder::build(renderer::vk::context ctx,
+                                                VkShaderStageFlags shader_stages, void *pNext,
+                                                VkDescriptorSetLayoutCreateFlags flags)
+    -> tl::expected<VkDescriptorSetLayout, error> {
   for (auto &b : bindings) {
     b.stageFlags |= shader_stages;
   }
@@ -120,11 +227,4 @@ auto surge::vk_atom::descriptor::desc_builder::build(
 void surge::vk_atom::descriptor::destroy_desc_layout(renderer::vk::context ctx,
                                                      VkDescriptorSetLayout layout) {
   vkDestroyDescriptorSetLayout(ctx->device, layout, renderer::vk::get_alloc_callbacks());
-}
-
-void surge::vk_atom::descriptor::update_desc_sets(renderer::vk::context ctx, u32 write_count,
-                                                  const VkWriteDescriptorSet *desc_writes,
-                                                  u32 copy_count,
-                                                  const VkCopyDescriptorSet *desc_copies) {
-  vkUpdateDescriptorSets(ctx->device, write_count, desc_writes, copy_count, desc_copies);
 }
